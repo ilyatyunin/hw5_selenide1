0da6d50572f80f1b9208a830d1cd63b36732e81d
Ilya Tyunin <tomslavg96@gmail.com>
2023-04-07 16:10:20 +0300

    hw 5 selenide 1 ready

---
+++ src/test/java/ru/betboom/JUnit5ToSoftAssertionsInSelenide.java
@@ -0,0 +1,20 @@
+package ru.betboom;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class JUnit5ToSoftAssertionsInSelenide {
+    @Test
+    void SelenideSoftAssertionsHaveJunit5Example() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+        $("#wiki-pages-box").shouldHave(text("Soft")).shouldHave(text("Assertions"));
+
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("#user-content-3-using-junit5-extend-test-class").parent().shouldHave(text("Using JUnit5 extend test class"));
+    }
+}
